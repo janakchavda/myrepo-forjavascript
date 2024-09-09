bfe6a028fec81e6d8b253d3bf506e84d416ac6dd
janakchavda <janakchavda97@gmail.com>
2024-09-09 17:18:58 +0000

    janak

---
+++ maripelisitejsma/index.js
@@ -0,0 +1,18 @@
+const accountId=14444
+let accountEmail = "janakchavdaa97@gmail.com"  
+var accountPasword = "12345"
+accountCity = "surat"
+
+// accountId = 2
+accountEmail= "chavdajanak@gmail.com"
+accountCity = "velanja"
+accountPasword = "4321"
+
+
+//console.log(accountEmail);
+//console.table([accountEmail, accountCity, accountPasword])
+
+
+
+
+
